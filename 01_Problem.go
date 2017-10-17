c6eb18a91c98a35657ae172a66a8ed71943b9f3a
CONOR RAFTERY - Student <G00274094@gmit.ie>
2017-10-17 12:49:57 +0100

    Problem 1 Finished

---
+++ 01_Problem.go
@@ -0,0 +1,23 @@
+// Problem 1
+// Conor Raftery 12/10/17
+
+package main
+
+import (
+	"fmt"
+	"net/http"
+)
+
+func requestHandler(w http.ResponseWriter, r *http.Request) {
+
+	//Browser renders html tags
+	w.Header().Set("Content-Type","text/html");
+
+	//Output to browser
+	fmt.Fprintln(w, "Guessing Game")
+}
+
+func main() {
+	http.HandleFunc("/", requestHandler)
+	http.ListenAndServe(":8080", nil)
+}
